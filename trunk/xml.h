fc2a1ed4bc3780408f375c52a6fc773dddccd1b6
dbalster <dbalster@3cde1cb9-281b-7e81-5dea-f7cfb64f2c34>
2010-06-24 20:49:17 +0000

    changed: formatted code changed: memset zero for allocator fixed: manifest was turned off

--- trunk/xml.h
+++ trunk/xml.h
@@ -1,31 +1,31 @@
 /*
- * dbalster's XML DOM parser
- *
- * Copyright (c) Daniel Balster
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Daniel Balster nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY DANIEL BALSTER ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL DANIEL BALSTER BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+* dbalster's XML DOM parser
+*
+* Copyright (c) Daniel Balster
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of Daniel Balster nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY DANIEL BALSTER ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL DANIEL BALSTER BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #ifndef DBALSTER_XML_H
 #define DBALSTER_XML_H
@@ -70,23 +70,23 @@ struct _XmlSizeofHint
 // linear linked list of Xml attributes
 struct _XmlAttribute
 {
-	const char*   name;
-	const char*   content;
-	XmlAttribute*	next;
+  const char*   name;
+  const char*   content;
+  XmlAttribute*	next;
 };
 
 // Xml element holds a link to its parent element, a list of its children elements
 //and a list of this element's atributes.
 struct _XmlElement
 {
-	const char*   name;
-	const char*   content;
-	XmlElement*		tail;					// link to last node
-	XmlElement*		next;					// link to next element or null if last element
-	XmlElement*		parent;				// link to parent or null if root element
-	XmlElement*		elements;			// children elements (or null if no children)
-	XmlAttribute*	attributes;		// element attributes (or null...)
-	void*         userdata;			// C-like userdata.
+  const char*   name;
+  const char*   content;
+  XmlElement*		tail;					// link to last node
+  XmlElement*		next;					// link to next element or null if last element
+  XmlElement*		parent;				// link to parent or null if root element
+  XmlElement*		elements;			// children elements (or null if no children)
+  XmlAttribute*	attributes;		// element attributes (or null...)
+  void*         userdata;			// C-like userdata.
 };
 
 // error handler
